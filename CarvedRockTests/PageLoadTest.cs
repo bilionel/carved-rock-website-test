cb613f73aee5efe1c3475f751e97082b3356afd7
Michael Bailey <michaelbailey@infojolt.com>
2021-05-14 08:51:59 -0700

    Add test case for post carabiners

--- CarvedRockTests/PageLoadTest.cs
+++ CarvedRockTests/PageLoadTest.cs
@@ -17,6 +17,7 @@ namespace CarvedRockTests
 
         [TestCase("")]
         [TestCase("contact")]
+        [TestCase("post/carabiners/")]
         public async Task Homepage_Ok_Response_Code(string relativePath)
         {
             var response = await this.HttpClient.GetAsync($"http://ec2-18-133-221-163.eu-west-2.compute.amazonaws.com/{relativePath}");
